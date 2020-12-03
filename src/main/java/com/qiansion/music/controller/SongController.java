7b89d4b98dd34c73db63edc866d54a98dde976a8
wangqianyi <472789018@qq.com>
2020-12-03 18:28:58 +0800

    音乐界面的管理

--- src/main/java/com/qiansion/music/controller/SongController.java
+++ src/main/java/com/qiansion/music/controller/SongController.java
@@ -1,6 +1,7 @@
 package com.qiansion.music.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.qiansion.music.domain.Singer;
 import com.qiansion.music.domain.Song;
 import com.qiansion.music.service.SongService;
 import com.qiansion.music.util.Consts;
@@ -22,48 +23,52 @@ public class SongController {
     @Autowired
     private SongService songService;
     /**
+     * 更新歌曲图片
+     */
+    @PostMapping("/updateSongPic")
+    public Object insert(@RequestParam("file") MultipartFile avatorFile, @RequestParam("id") int id){
+        JSONObject jsonObject = new JSONObject();
+        if(avatorFile.isEmpty()){
+            jsonObject.put(Consts.CODE,0);
+            jsonObject.put(Consts.MSG,"歌曲图片上传失败");
+            return jsonObject;
+        }
+        // 防止文件名重复:文件名 = 当前时间毫秒+原来的文件名
+        String fileName = System.currentTimeMillis()+avatorFile.getOriginalFilename();
+        // 如果文件路径不存在则新增
+        String filePath = System.getProperty("user.dir")+System.getProperty("file.separator")+"img"+ System.getProperty("file.separator")+"SongPic";
+        File file1 = new File(filePath);
+        if(!file1.exists()){
+            file1.mkdir();
+        }
+        // 实际的文件地址
+        File dest = new File(filePath+System.getProperty("file.separator")+fileName);
+        // 存储到数据库的相对文件地址
+        String storeAvatorPath = "/img/SongPic/"+fileName;
+        try {
+            avatorFile.transferTo(dest);
+            Song song = new Song();
+            song.setId(id);
+            song.setPic(storeAvatorPath);
+            boolean flag = songService.update(song);
+            if(flag){
+                jsonObject.put(Consts.CODE,1);
+                jsonObject.put(Consts.MSG,"上传成功");
+                jsonObject.put("pic",storeAvatorPath);
+            }else{
+                jsonObject.put(Consts.CODE,1);
+                jsonObject.put(Consts.MSG,"上传失败");
+            }
+        } catch (IOException e) {
+            jsonObject.put(Consts.CODE,1);
+            jsonObject.put(Consts.MSG,"上传失败"+e.getMessage());
+        }finally {
+            return jsonObject;
+        }
+    }
+    /**
      * 增加
      */
-//    @PostMapping("/add")
-//    public Object insert(@RequestBody Song song, @RequestParam("file")MultipartFile mpFile){
-//        JSONObject jsonObject =new JSONObject();
-//        System.out.println(song.toString());
-//        if(mpFile.isEmpty()){
-//            jsonObject.put(Consts.CODE,0);
-//            jsonObject.put(Consts.MSG,"歌曲上传失败");
-//            return jsonObject;
-//        }
-//        // 防止文件名重复:文件名 = 当前时间毫秒+原来的文件名
-//        String fileName = System.currentTimeMillis()+mpFile.getOriginalFilename();
-//        // 如果文件路径不存在则新增
-//        String filePath = System.getProperty("user.dir")+System.getProperty("file.separator")+"song";
-//        File file1 = new File(filePath);
-//        if(!file1.exists()){
-//            file1.mkdir();
-//        }
-//        // 实际的文件地址
-//        File dest = new File(filePath+System.getProperty("file.separator")+fileName);
-//        // 存储到数据库的相对文件地址
-//        String storeSongPath = "/song/"+fileName;
-//        try {
-//            mpFile.transferTo(dest);
-//            song.setUrl(storeSongPath);
-//            boolean flag = songService.insert(song);
-//            if(flag){
-//                jsonObject.put(Consts.CODE,1);
-//                jsonObject.put(Consts.MSG,"添加成功");
-//                jsonObject.put("pic",storeSongPath);
-//            }else{
-//                jsonObject.put(Consts.CODE,1);
-//                jsonObject.put(Consts.MSG,"添加失败");
-//            }
-//        } catch (IOException e) {
-//            jsonObject.put(Consts.CODE,1);
-//            jsonObject.put(Consts.MSG,"歌曲存储失败"+e.getMessage());
-//        }finally {
-//            return jsonObject;
-//        }
-//    }
     @ResponseBody
     @PostMapping("/add")
     public Object addSong(HttpServletRequest req, @RequestParam("file") MultipartFile mpfile){
@@ -118,6 +123,53 @@ public class SongController {
             return jsonObject;
         }
     }
+
+    /**
+     * 更新歌曲
+     */
+    @ResponseBody
+    @PostMapping("/updateSongUrl")
+    public Object updateSongUrl(@PathVariable("id") Integer id, @RequestParam("file") MultipartFile mpfile){
+        JSONObject jsonObject = new JSONObject();
+        if (mpfile.isEmpty()) {
+            jsonObject.put("code", 0);
+            jsonObject.put("msg", "音乐上传失败！");
+            return jsonObject;
+        }
+        String fileName =System.currentTimeMillis()+mpfile.getOriginalFilename();
+        String filePath = System.getProperty("user.dir") + System.getProperty("file.separator") + "song";
+        File file1 = new File(filePath);
+        if (!file1.exists()){
+            file1.mkdir();
+        }
+
+        File dest = new File(filePath + System.getProperty("file.separator") + fileName);
+        String storeUrlPath = "/song/"+fileName;
+        try {
+            FileUtil.deleteSongFile(songService.getUrlofSongById(id));
+            mpfile.transferTo(dest);
+            Song song = new Song();
+            song.setId(id);
+            song.setUrl(storeUrlPath);
+            boolean res = songService.insert(song);
+            if (res) {
+                jsonObject.put("code", 1);
+                jsonObject.put("avator", storeUrlPath);
+                jsonObject.put("msg", "上传成功");
+                return jsonObject;
+            } else {
+                jsonObject.put("code", 0);
+                jsonObject.put("msg", "上传失败");
+                return jsonObject;
+            }
+        } catch (IOException e) {
+            jsonObject.put("code", 0);
+            jsonObject.put("msg", "上传失败" + e.getMessage());
+            return jsonObject;
+        } finally {
+            return jsonObject;
+        }
+    }
     /**
      * 修改
      */
@@ -145,10 +197,10 @@ public class SongController {
         JSONObject jsonObject =new JSONObject();
         if(songService.delete(id)){
             jsonObject.put(Consts.CODE,1);
-            jsonObject.put(Consts.MSG,"添加成功");
+            jsonObject.put(Consts.MSG,"删除成功");
         }else{
             jsonObject.put(Consts.CODE,0);
-            jsonObject.put(Consts.MSG,"添加失败");
+            jsonObject.put(Consts.MSG,"删除失败");
         }
         return jsonObject;
     }
